883c345ee92806d425d0337d4ee31947fca10860
Vladimir Ruzic <vladimir@ruzic.info>
2018-10-22 23:41:10 +0200

    sending token function implemented

--- app/common/ethereum/Ethereum.js
+++ app/common/ethereum/Ethereum.js
@@ -1,13 +1,16 @@
 const ethers = require('ethers');
 
 const { encryptedWallet, password, contractAddress, abi } = require('../../../configuration/wallet');
+const logger = require('app/common/log/logger.service')
 
 class Ethereum {
   static async create() {
     const provider = ethers.getDefaultProvider();
-    const wallet = await ethers.Wallet.fromEncryptedJson(JSON.stringify(encryptedWallet), password);
+    const walletWithoutProvider = await ethers.Wallet.fromEncryptedJson(JSON.stringify(encryptedWallet), password);
 
-    const contract = new ethers.Contract(contractAddress, abi, provider);
+    const wallet = walletWithoutProvider.connect(provider);
+
+    const contract = new ethers.Contract(contractAddress, abi, wallet);
 
     return new Ethereum(provider, wallet, contract);
   }
@@ -16,11 +19,12 @@ class Ethereum {
     this.provider = provider;
     this.wallet = wallet;
     this.contract = contract;
+    this.numberOfDecimals = 18;
   }
   async checkTransaction(txHash) {
     const transaction = await this.provider.getTransaction('0x653e8c35de74b8dfbce5e16c8098fae9203a858f39d9c86b946c8e94bbc5962d');
 
-    console.log(transaction);
+    logger.info(transaction);
   }
   async checkBalance() {
     try{
@@ -29,6 +33,15 @@ class Ethereum {
 
     } catch(error) {
       console.log(error);
+    }
+  }
+  async sendToken(address, value) {
+    try {
+      const valueToSend = ethers.utils.parseUnits(value.toString(), this.numberOfDecimals);
+      const result = await this.contract.transfer(address, valueToSend);
+    } catch(error) {
+      logger.error('Error happened');
+      logger.error(error);
     }
   }
 }
