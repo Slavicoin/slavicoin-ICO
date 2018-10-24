b82329a4f6ea925e6286602d391adacf32c3e55c
Vladimir Ruzic <vladimir@ruzic.info>
2018-10-24 23:05:22 +0200

    updage health route

--- app/api/health/get/health.route.js
+++ app/api/health/get/health.route.js
@@ -1,5 +1,22 @@
-function healthRoute(req, res) {
-  res.status(200).json('OK');
+const databaseService = require('app/database/database.service');
+
+async function healthRoute(req, res) {
+  const { database } = databaseService.get();
+
+  try {
+    await database.raw('SELECT 1;');
+
+    res.status(200).json({
+      health: {
+        api: true,
+        memory: true
+      }
+    });
+
+  } catch(error) {
+    res.status(500).json(error);
+  }
+
 }
 
 module.exports = function (app) {
