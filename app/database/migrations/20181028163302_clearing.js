c4833e8b86b74127656f7ace772eef51d718c29c
Vladimir Ruzic <vladimir@ruzic.info>
2018-10-28 22:42:34 +0100

    Addding clearing SQL

--- app/database/migrations/20181028163302_clearing.js
+++ app/database/migrations/20181028163302_clearing.js
@@ -31,6 +31,17 @@ exports.up = function(knex, Promise) {
         PRIMARY KEY ("id"),
         CONSTRAINT "c_type_fk" FOREIGN KEY ("type") REFERENCES "public"."clearing_type" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
       );
+    CREATE TABLE
+      "public"."clearing_transactions" (
+        "clearing_id" int2 NOT NULL,
+        "incoming_id" int2 NOT NULL,
+        "outgoing_id" int2 NOT NULL,
+        PRIMARY KEY ("clearing_id", "incoming_id", "outgoing_id"),
+        CONSTRAINT "ct_clearing_fk" FOREIGN KEY ("clearing_id") REFERENCES "public"."clearing" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+        CONSTRAINT "ct_incoming_fk" FOREIGN KEY ("incoming_id") REFERENCES "public"."transaction" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
+        CONSTRAINT "ct_outgoing_fk" FOREIGN KEY ("outgoing_id") REFERENCES "public"."transaction" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+
+      );
   `;
   return knex.raw(sql);
 };
@@ -40,6 +51,7 @@ exports.down = function(knex, Promise) {
     DROP SEQUENCE IF EXISTS "public"."clearing_id_seq" CASCADE; 
     DROP SEQUENCE IF EXISTS "public"."clearing_type_id_seq" CASCADE; 
     
+    DROP TABLE IF EXISTS "public"."clearing_transactions" CASCADE; 
     DROP TABLE IF EXISTS "public"."clearing" CASCADE; 
     DROP TABLE IF EXISTS "public"."clearing_type" CASCADE; 
   `;
